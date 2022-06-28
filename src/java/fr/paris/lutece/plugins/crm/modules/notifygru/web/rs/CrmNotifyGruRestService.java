a40d281aab4ca08eb87ae299a8d7dad2546a4db5
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2022-06-28 16:17:43 +0200

    Update fir Lutece Core 7

--- src/java/fr/paris/lutece/plugins/crm/modules/notifygru/web/rs/CrmNotifyGruRestService.java
+++ src/java/fr/paris/lutece/plugins/crm/modules/notifygru/web/rs/CrmNotifyGruRestService.java
@@ -71,7 +71,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 @Path( RestConstants.BASE_PATH + CrmNotifyGruConstants.API_PATH + CrmNotifyGruConstants.VERSION_PATH + CrmNotifyGruConstants.PLUGIN_NAME )
